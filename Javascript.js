217513b8d679810fedc2626b1a548869af7e5d46
abhishek172001 <95580992+abhishek172001@users.noreply.github.com>
2022-01-07 22:57:58 +0530

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,20 @@
+function Name()
+{
+    document.getElementById('name').style.backgroundColor='#00FFFF';
+}
+function Number()
+{
+    document.getElementById('number').style.backgroundColor='#7CFC00';
+}
+function Email()
+{
+    document.getElementById('email').style.backgroundColor='#FFB6C1';
+}
+function Search1()
+{
+    document.getElementById('search1').style.backgroundColor='#FF6347';
+}
+function Text1()
+{
+    document.getElementById('text1').style.backgroundColor='#FF8C00';
+}
